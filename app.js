5f890cdd40abf40e6588250a38a835d073e7086d
Mark Harding <markharding@Marks-MacBook-Pro-4.local>
2015-01-16 20:34:18 -0500

    app basic

--- app.js
+++ app.js
@@ -8,3 +8,22 @@ var engine = require('ejs-locals');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+
+app.set('port', process.env.PORT || 8088);
+app.engine('ejs', engine);
+app.set('views', path.join( __dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true }));
+app.use(bodyParser.json());
+app.use( express.static( path.join( __dirname, 'public' )));
+
+app.use(cookieParser());
+app.use(session({secret: 'hardingm'}));
+
+routes.init(function() {
+	app.get( '/', routes.index );
+
+	http.createServer( app ).listen( app.get( 'port' ), function(){
+		console.log( 'Open browser to http://localhost:' + app.get( 'port' ));
+	} );
+});
