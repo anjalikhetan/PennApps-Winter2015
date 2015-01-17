86134cb154f248004192b360b99cc83a0d463189
Alex Harelick <harelick@seas.upenn.edu>
2015-01-16 21:13:19 -0500

    app.js runs without errors

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ var routes = require ('./routes');
 var http = require('http');
 var path = require('path');
 var app = express();
-var engine = require('ejs-locals');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
 app.set('port', process.env.PORT || 8088);
-app.engine('ejs', engine);
 app.set('views', path.join( __dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true }));
@@ -18,7 +16,10 @@ app.use(bodyParser.json());
 app.use( express.static( path.join( __dirname, 'public' )));
 
 app.use(cookieParser());
-app.use(session({secret: 'hardingm'}));
+app.use(session({secret: 'hardingm',
+				saveUninitialized: true,
+                resave: true}
+));
 
 routes.init(function() {
 	app.get( '/', routes.index );
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@
 
 // We export the init() function to initialize
 // our KVS values
+
+exports.init = function(callback) {
+	callback();
+}
 exports.index = function(req, res) {
 	res.render('signup');
 }
