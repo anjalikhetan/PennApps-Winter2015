b58297a1fd09cc8ee5f5957ac042b0322c964f8b
Mark Harding <markharding@Marks-MacBook-Pro-4.local>
2015-01-16 20:22:58 -0500

    added app

---
+++ app.js
@@ -0,0 +1,10 @@
+
+var express = require('express');
+var routes = require ('./routes');
+var http = require('http');
+var path = require('path');
+var app = express();
+var engine = require('ejs-locals');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
