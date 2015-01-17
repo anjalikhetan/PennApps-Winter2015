d54ce183ab3b2baae88d03d0f2246320b3ab31f2
mark345 <mjharding3@gmail.com>
2015-01-16 23:50:59 -0500

    aws tables work for login

--- routes/index.js
+++ routes/index.js
@@ -21,21 +21,21 @@ exports.signin = function(req, res) {
 
 exports.createAccount = function(req, res) {
 	// check database for existing username
-	// if username doesn't exist
-	console.log("createAccount opened.");
 	var userID = req.body.inputPhoneNumber;
 	console.log("userID = " + userID);
 	users.exists(userID, function(err, data) {
 
 		if (data) {
-			console.log("This phone number already existed.");
 			res.render('signup', {message: "Username exists, choose another"});
 		} else {
 			console.log("This phone number is new to the table.");
 			var json = {phoneNumber: userID};
 			 /*Need to update the value object*/
+			 console.log("passing var creation");
 
-			users.put(userID, JSON.stringify(json), 0, function(err, data) {
+			users.put(userID, JSON.stringify(json), "0", function(err, data) {
+				console.log(err);
+				console.log("i'm putting " + userID + " " + JSON.stringify(json));
 			});
 			res.render('home');
 		}
